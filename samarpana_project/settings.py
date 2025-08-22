a7740cf249e7c4701c58294005f14fbd502016b3
Prithvi Raj <prithvirajbl14@gmail.com>
2025-08-23 02:16:09 +0530

    Update project with latest changes

--- samarpana_project/settings.py
+++ samarpana_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
 import os
 
 
@@ -26,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(73k20$r=ow6j*k*klvu9ip_5)knnuofhdcn2-@((@ugg5b^d+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['myapp.onrender.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -76,18 +75,18 @@ WSGI_APPLICATION = 'samarpana_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3'
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Samarpana@12202426',
+        'HOST': 'db.sphefvvkkgkothdaiudr.supabase.co',
+        'PORT': '5432',
+    }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
